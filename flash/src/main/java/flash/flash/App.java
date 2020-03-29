209684d2e1f78dfdc3b6ef9c4c0607b7318a3a68
Abhijit Biradar <biradar.abhijit@gmail.com>
2020-03-29 17:38:46 +0530

    project setup  commit 1

---
+++ flash/src/main/java/flash/flash/App.java
@@ -0,0 +1,13 @@
+package flash.flash;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
